6cd521a783acf417a793aeeeaea7cd0570ab6c07
Henriq-Garcia <botzinso@gmail.com>
2024-07-23 11:46:26 -0300

    test: multiple clients connection

--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -5,3 +5,27 @@ const client = new SimpleWsClient("ws://localhost:8080");
 client.on("open", () => {
     console.log("conexão iniciada")
 })
+
+const client1 = new SimpleWsClient("ws://localhost:8080");
+
+client1.on("open", () => {
+    console.log("conexão iniciada")
+})
+
+const client2 = new SimpleWsClient("ws://localhost:8080");
+
+client2.on("open", () => {
+    console.log("conexão iniciada")
+})
+
+const client3 = new SimpleWsClient("ws://localhost:8080");
+
+client3.on("open", () => {
+    console.log("conexão iniciada")
+})
+
+const client4 = new SimpleWsClient("ws://localhost:8080");
+
+client4.on("open", () => {
+    console.log("conexão iniciada")
+})
